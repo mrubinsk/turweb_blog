3a21e31cba8070f565f93423a24e7326f2481a74
Michael J Rubinsky <mrubinsk@horde.org>
2017-01-10 08:39:46 -0500

    Temporarily use flickr badge.

--- app/views/shared/_photostream.html.php
+++ app/views/shared/_photostream.html.php
@@ -1,4 +1,8 @@
 <div>
   <h3>Photos</h3>
-  <script type="text/javascript" src="http://h4.theupstairsroom.com/horde/services/ajax.php/ansel/embed?count=10&user=mike&thumbsize=mini&thumbtype=SquareThumb&container=4e63c5bd0d76f"></script><div id="4e63c5bd0d76f"></div>
+ <!-- <script type="text/javascript" src="http://h4.theupstairsroom.com/horde/services/ajax.php/ansel/embed?count=10&user=mike&thumbsize=mini&thumbtype=SquareThumb&container=4e63c5bd0d76f"></script><div id="4e63c5bd0d76f"></div>-->
+ <style type="text/css">
+.flickrimg {border: 1px solid #666666 !important; padding:1px; margin:2px;}
+#flickr_badge_wrapper {width:250px;text-align:left}
+</style><div id="flickr_badge_wrapper"><script type="text/javascript" src="http://www.flickr.com/badge_code.gne?count=12&display=random&size=square&nsid=25537240@N02&raw=1"></script><center><small>create with <a href="http://www.flickrbadge.com">flickr badge</a>.</small></center></div>
 </div>
