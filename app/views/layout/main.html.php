3f7ac464c2ecd8abf7d8754e0a385b2f5824cc11
Michael J Rubinsky <mrubinsk@horde.org>
2017-03-24 15:37:36 -0400

    Use https

--- app/views/layout/main.html.php
+++ app/views/layout/main.html.php
@@ -9,19 +9,19 @@
   <meta name="description" content="">
   <meta name="author" content="Michael Rubinsky">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-  <link rel="stylesheet" href="<?php echo 'http://' . $this->host_base?>/css/normalize.css">
-  <link rel="stylesheet" href="<?php echo 'http://' . $this->host_base?>/css/skeleton.css">
-  <link rel="stylesheet" href="<?php echo 'http://' . $this->host_base?>/css/custom.css">
-  <link rel="stylesheet" type="text/css" href="http://h4.theupstairsroom.com/horde/js/syntaxhighlighter/styles/shThemeEclipse.css"/>
-  <link rel="stylesheet" type="text/css" href="http://h4.theupstairsroom.com/horde/js/syntaxhighlighter/styles/shCoreEclipse.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+  <link rel="stylesheet" href="<?php echo 'https://' . $this->host_base?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo 'https://' . $this->host_base?>/css/skeleton.css">
+  <link rel="stylesheet" href="<?php echo 'https://' . $this->host_base?>/css/custom.css">
+  <link rel="stylesheet" type="text/css" href="https://h4.theupstairsroom.com/horde/js/syntaxhighlighter/styles/shThemeEclipse.css"/>
+  <link rel="stylesheet" type="text/css" href="https://h4.theupstairsroom.com/horde/js/syntaxhighlighter/styles/shCoreEclipse.css"/>
   <link rel="icon" type="image/png" href="images/favicon.png">
-  <script type="text/javascript" src="http://h4.theupstairsroom.com/horde/js/prototype.js"></script>
-  <script type="text/javascript" src="http://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/shCore.js"></script>
-  <script type="text/javascript" src="http://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/shAutoloader.js"></script>
+  <script type="text/javascript" src="https://h4.theupstairsroom.com/horde/js/prototype.js"></script>
+  <script type="text/javascript" src="https://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/shCore.js"></script>
+  <script type="text/javascript" src="https://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/shAutoloader.js"></script>
   <script type="text/javascript">
       document.observe('dom:loaded', function() {
-          var path = 'http://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/';
+          var path = 'https://h4.theupstairsroom.com/horde/js/syntaxhighlighter/scripts/';
           SyntaxHighlighter.autoloader(
             'applescript ' + path + 'shBrushAppleScript.js',
             'bash shell ' + path + 'shBrushBash.js',
@@ -41,7 +41,7 @@
       function updatePreviously(page)
       {
           new Ajax.Updater('previously',
-                       '<?php echo 'http://' . $this->host_base . '/gotopage/'?>' + page);
+                       '<?php echo 'https://' . $this->host_base . '/gotopage/'?>' + page);
           return false;
       }
   </script>
@@ -50,7 +50,7 @@
   <div id="header-wrapper">
     <div id="header" class="container">
       <div id="logo">
-        <h1><a href="http://<?php echo $this->host_base?>">theUpstairsRoom</a></h1>
+        <h1><a href="https://<?php echo $this->host_base?>">theUpstairsRoom</a></h1>
         <span>News and Ramblings on Horde development</span>
       </div>
     </div>
@@ -61,7 +61,7 @@
       <div class="title">
         <strong>{ Hi! I'm Mike }</strong>
       </div>
-      <div class="content"><!-- <img src="http://www.gravatar.com/avatar/fb930e44282b804151c29212eebaa883?s=80" /> -->
+      <div class="content"><!-- <img src="https://www.gravatar.com/avatar/fb930e44282b804151c29212eebaa883?s=80" /> -->
         I'm a core developer with <a href="https://horde.org" alt="The Horde Project">The Horde Project</a> and a
         founding parter of <a href="https://horde.org/services">Horde LLC</a> - the company behind the world's most
         flexible groupware platform. This is my personal blog full of random
